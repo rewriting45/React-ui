679e90026028db825028818b58fd9b86ba793ae9
rewriting45 <rewriting45@163.com>
2024-05-26 15:16:59 +0800

    feat: 更新FONTICON组件

--- src/components/Icon/icon.stories.js
+++ src/components/Icon/icon.stories.js
@@ -1,5 +1,7 @@
 import Icon from ".";
 import { ReactComponent as Smile } from "../../assets/icons/smile.svg"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCommentDots} from "@fortawesome/free-solid-svg-icons";
 
 export default {
     title: "UI 组件/Icon",
@@ -23,5 +25,26 @@ export const CustomColors = () => {
         <Icon icon={Smile} color="black"/>
         <Icon icon={Smile} color="skyblue"/>
         <Icon icon={Smile} color="green"/>
+    </div>
+}
+
+export const FontAwesome = () => {
+    return <div className="row-elements">
+        <FontAwesomeIcon icon={faCommentDots}></FontAwesomeIcon>
+    </div>
+}
+
+export const FontAwesomeColor = () => {
+    return <div className="row-elements">
+        <FontAwesomeIcon icon={faCommentDots} color="red"></FontAwesomeIcon>
+    </div>
+}
+
+export const FontAwesomeSize = () => {
+    return <div className="row-elements">
+        <FontAwesomeIcon icon={faCommentDots} color="red" fontSize="24px"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faCommentDots} color="red" fontSize="32px"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faCommentDots} color="red" fontSize="40px"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faCommentDots} color="red" fontSize="48px"></FontAwesomeIcon>
     </div>
 }
